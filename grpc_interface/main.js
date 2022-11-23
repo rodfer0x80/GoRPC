630479311c113aae0c018666301394886b8ccb64
rodfer0x80 <rodfer0x80@proton.me>
2022-11-23 15:28:15 +0000

    [*] Switching iface from pyqt6 to ts-js-react

---
+++ grpc_interface/main.js
@@ -0,0 +1,6 @@
+// Start server on localhost:3420
+// Open browser on https://localhost:3420 
+// Display table of connections on left side
+// Show admin option on top bar
+// Display table of embed-admin interactions on right side
+// Use typescript and react for this in FP>OOP style
